5367f23d36a21c3258be93e30fb49ade8aba7eae
satyam chauhan <satyamchauhan22@gmail.com>
2020-03-14 12:41:27 +0530

    third commit

--- register_login/settings.py
+++ register_login/settings.py
@@ -115,3 +115,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_URL = '/static/'
 STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['static']))
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
