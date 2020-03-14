2ba3e6d6b718d154fc6ecd47abede425c412ac64
satyam chauhan <satyamchauhan22@gmail.com>
2020-03-14 12:04:09 +0530

    third commit

--- register/urls.py
+++ register/urls.py
@@ -1,7 +1,7 @@
 
 from django.urls import path
 from . import views
-urlpatterns = [path('https://registerlogin11.herokuapp.com/',views.home),
-               path('https://registerlogin11.herokuapp.com/register/',views.reg),
-               path('https://registerlogin11.herokuapp.com/loggedin/',views.loggedin)
+urlpatterns = [path('',views.home),
+               path('register/',views.reg),
+               path('loggedin/',views.loggedin)
                ]
